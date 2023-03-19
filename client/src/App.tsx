a225a4ef0851969ea65a2d063fef5ed911bce44c
Vasyl Boyko <vboiko0@gmail.com>
2023-03-19 18:54:59 +0200

    add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button, Link as MuiLink, Stack } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
@@ -9,6 +10,7 @@ import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
 import { trpc } from "./utils/trpc";
 import { Positions } from "./views/Positions";
+import { Routes, Route, Outlet, Link as RouterLink } from "react-router-dom";
 
 export function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -21,26 +23,54 @@ export function App() {
       ],
     })
   );
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <>
-          <CssBaseline />
-          <Container maxWidth="xl">
-            <Box sx={{ height: "100vh" }}>
-              <AppBar position="static">
-                <Toolbar variant="dense">
-                  <Typography variant="h6" color="inherit" component="div">
-                    Job Apply Assist
-                  </Typography>
-                </Toolbar>
-              </AppBar>
-
-              <Positions />
-            </Box>
-          </Container>
-        </>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Positions />} />
+            <Route path="positions" element={<Positions />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
       </QueryClientProvider>
     </trpc.Provider>
   );
 }
+
+const Layout = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Box sx={{ height: "100vh" }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Typography variant="h6" color="inherit" component="div">
+                Job Apply Assist
+              </Typography>
+
+              <Box ml={4}></Box>
+
+              <Stack direction="row" spacing={2}>
+                <Button
+                  color="inherit"
+                  variant="text"
+                  component={RouterLink}
+                  to="/"
+                >
+                  Positions
+                </Button>
+              </Stack>
+            </Toolbar>
+          </AppBar>
+
+          <Outlet />
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+const NoMatch = () => <div>Page not found</div>;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -9,6 +10,8 @@ import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
